3c98a865963e91d88b0d681ab5cdc6dafe4b774d
Tommy Mynn Son <tommy.m.son@gmail.com>
2021-10-14 02:24:20 -0700

    Finish Node Postgres

---
+++ bootcamp_app/students.js
@@ -0,0 +1,26 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'tommyson',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+const cohort = process.argv[2] + '%';
+const limit = Number(process.argv[3]);
+
+pool.query(`
+SELECT students.id, students.name, cohorts.name AS cohort_name
+FROM students
+JOIN cohorts
+ON students.cohort_id = cohorts.id
+WHERE cohorts.name LIKE '${cohort}'
+LIMIT ${limit};
+`)
+  .then(res => {
+    res.rows.forEach(user => {
+      console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+    });
+  })
+  .catch(err => console.error('query error', err.stack));
